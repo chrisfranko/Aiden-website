db56974b05e18bb103725271d848ec6182456057
snogcel <ledniknoj@gmail.com>
2014-04-11 19:39:21 -0600

    v.2 of website
    placeholder content, updated nav, etc

---
+++ js/aiden.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+// simple jMenu plugin called
+$("#jMenu").jMenu();
+
+// more complex jMenu plugin called
+$("#jMenu").jMenu({
+  ulWidth : 'auto',
+  effects : {
+    effectSpeedOpen : 300,
+    effectTypeClose : 'slide'
+  },
+  animatedText : false
+});
+	
+// custom nav handler
+$('.navLink').click(function() {
+	var rel = $(this).attr('rel');
+	$('.active').addClass('hidden').removeClass('active');
+	$('.selected').removeClass('selected');
+	$('#'+rel+'').addClass('active').show( "fade", { direction: "down" }, "slow" ).removeClass('hidden');
+	$(this).addClass('selected');
+});
+	
+});
